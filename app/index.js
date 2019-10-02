46c003f6dde606945466938288b21e25657b3032
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-10-02 13:28:02 +0700

    setup static files

--- app/index.js
+++ app/index.js
@@ -40,6 +40,6 @@ app.use(bodyParser.json());
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "pug");
 
-app.use(express.static("../public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 module.exports = app;
